3851f8c22b9f1cf2f4355374e2a78ed957973307
Matt Anis <webdev.omis@gmail.com>
2026-07-25 01:43:01 +0100

    Exercise: 1. Repository and the Database

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
-const { Sequelize, QueryTypes, Model, DataTypes } = require("sequelize");
-const express = require("express");
-const app = express();
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
@@ -12,54 +11,14 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
-class Note extends Model {}
-Note.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    important: {
-      type: DataTypes.BOOLEAN,
-    },
-    date: {
-      type: DataTypes.DATE,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "note",
-  },
-);
-
-app.use(express.json());
-
-app.get("/api/notes", async (req, res) => {
-  const notes = await Note.findAll();
-  res.json(notes);
-});
-
-app.post("/api/notes", async (req, res) => {
+const main = async () => {
   try {
-    const note = await Note.create({ ...req.body, date: new Date() });
-    return res.json(note);
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    sequelize.close();
   } catch (error) {
-    return res.status(400).json({ error });
+    console.error("Unable to connect to the database:", error);
   }
-});
+};
 
-app.use((err, req, res, next) => {
-  res.status(400).json({ error: err.message });
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+main();
