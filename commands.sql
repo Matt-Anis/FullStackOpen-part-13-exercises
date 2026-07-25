d1c8a58beffdb39ae27bae9ed51773e168b0e32f
Matt Anis <webdev.omis@gmail.com>
2026-07-25 01:59:20 +0100

    Exercise: 2. Console connection

---
+++ commands.sql
@@ -0,0 +1,11 @@
+CREATE TABLE blogs (
+    id SERIAL PRIMARY KEY,
+    author text,
+    url text NOT NULL,
+    title text NOT NULL,
+    likes INTEGER DEFAULT 0
+);
+
+INSERT INTO blogs (author, title, url) VALUES ('Martin Fowler', 'Patterns of Enterprise Application Architecture', 'https://martinfowler.com/eaaCatalog/');
+INSERT INTO blogs (author, title, url) VALUES ('Dan Abramov', 'Overreacted: A JavaScript Blog', 'https://overreacted.io/');
+INSERT INTO blogs (author, title, url) VALUES ('Addy Osmani', 'Learning Patterns', 'https://www.patterns.dev/');
