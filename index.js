fd70615f857fe39b06527e3cc7c31d0da07ffc3d
Matt Anis <webdev.omis@gmail.com>
2026-07-27 20:13:15 +0100

    Exercise: 16. Authors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { connectToDatabase } = require("./util/db");
 const blogRouter = require("./controllers/blogs");
 const userRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
+const authorRouter = require("./controllers/authors");
 
 const errorHandler = (error, req, res, next) => {
   if (error.name === "SequelizeValidationError") {
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", userRouter);
 app.use("/api/login", loginRouter);
+app.use("/api/authors", authorRouter);
 
 app.use(errorHandler);
 app.use((err, req, res, next) => {
